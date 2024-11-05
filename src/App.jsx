817383f5d07cce992152a0259bba0f3ec5fac692
LDanielOchoa <daniel.ochoaa.cass@gmail.com>
2024-11-05 09:20:33 -0500

    cambios realizados

--- src/App.jsx
+++ src/App.jsx
@@ -72,7 +72,7 @@ export default function Component() {
     setIsLoading(true)
     setErrorMessage('')
     try {
-      const response = await fetch('http://excel-proviewer-production.up.railway.app/upload/', { 
+      const response = await fetch('https://excel-proviewer-production.up.railway.app/upload/', { 
         method: 'POST', 
         body: formData 
       })
